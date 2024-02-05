3f479ce7f68daf009f1f1b3bec4cbe1c7e0e82df
Crowdbotics <team@crowdbotics.com>
2024-02-05 22:59:25 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pricewatch-46017",
+  url: "https://pricewatch-46017.botics.co",
+  api: "https://pricewatch-46017.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
